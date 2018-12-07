d0d8a992112796e1ab0ca0001b47ca7a41a9aee5
Aluno <Aluno@DESKTOP-LDNVJ6K>
2018-12-07 11:14:06 -0300

    Felipe 07/12

--- VÔLEI/src/Main.java
+++ VÔLEI/src/Main.java
@@ -5,65 +5,85 @@ public class Main {
 	public static void main(String[] args) {
 		DAO<Tecnico> dao1 = new DAO<Tecnico>();
 		Tecnico T = new Tecnico();
-		T.setMatricula(2018001l);
+		T.setMatricula(201801l);
 		T.setNome("Josikleio");
 		T.setSexo("Masculino");
-		T.setIdade(35);
+		T.setIdade(39);
 		dao1.update(T);
-		Tecnico find1 = dao1.find(Tecnico.class, 2018001l);
+		Tecnico find1 = dao1.find(Tecnico.class, 201801l);
 		System.out.println(find1.getNome());
-		
+
+		DAO<Tecnico> daoTe = new DAO<Tecnico>();
+		Tecnico T2 = new Tecnico();
+		T2.setMatricula(201802l);
+		T2.setNome("Hugo Feitosa");
+		T2.setSexo("Masculino");
+		T2.setIdade(20);
+		dao1.update(T2);
+		Tecnico findTe = daoTe.find(Tecnico.class, 201802l);
+		System.out.println(findTe.getNome());
+
 		DAO<Time> dao2 = new DAO<Time>();
 		Time Tii = new Time();
-		Tii.setId(07l);
-		Tii.setNome("Barcelona");
+		Tii.setId(10l);
+		Tii.setNome("Flamengo");
 		Tii.setTecnico(T);
 		dao2.update(Tii);
-		Time find2 = dao2.find(Time.class, 07l);
+		Time find2 = dao2.find(Time.class, 10l);
 		System.out.println(find2.getNome());
-		
+
 		DAO<Time> daoT = new DAO<Time>();
 		Time Tii2 = new Time();
 		Tii2.setId(20l);
-		Tii2.setNome("Real madrid");
-		Tii2.setTecnico(T);
+		Tii2.setNome("Corinthians");
+		Tii2.setTecnico(T2);
 		dao2.update(Tii2);
 		Time findT = daoT.find(Time.class, 20l);
 		System.out.println(findT.getNome());
 
-		
-
 		DAO<Atleta> dao = new DAO<Atleta>();
 		Atleta A = new Atleta();
-		A.setMatricula(201719600020l);
-		A.setNome("Thiago Santos");
+		A.setMatricula(201719600021l);
+		A.setNome("Danilo Rodrigues");
 		A.setSexo("Masculino");
-		A.setIdade(17);
+		A.setIdade(16);
 		A.setTimes(Tii);
 		A.setTecnicos(T);
 		dao.update(A);
-		Atleta find = dao.find(Atleta.class, 201719600020l);
+		Atleta find = dao.find(Atleta.class, 201719600021l);
 		System.out.println(find.getNome());
+		
+		DAO<Atleta> daoA = new DAO<Atleta>();
+		Atleta A2 = new Atleta();
+		A2.setMatricula(201719600019l);
+		A2.setNome("Maria Clara");
+		A2.setSexo("Feminino");
+		A2.setIdade(15);
+		A2.setTimes(Tii2);
+		A2.setTecnicos(T2);
+		dao.save(A2);
+		Atleta findA = daoA.find(Atleta.class, 201719600019l);
+		System.out.println(findA.getNome());
 
 		DAO<Partida> dao4 = new DAO<Partida>();
 		Partida P = new Partida();
-		P.setId(001l);
+		P.setId(01l);
 		P.setTime_vencedor(Tii);
 		P.setTime_perdedor(Tii2);
 		dao4.update(P);
-		Partida find4 = dao4.find(Partida.class, 001l);
+		Partida find4 = dao4.find(Partida.class, 01l);
 		System.out.println(find4.getId());
 
 		DAO<Jogo> dao3 = new DAO<Jogo>();
 		Jogo J = new Jogo();
-		J.setId(0001l);
+		J.setId(02l);
 		J.setData(new Date());
-		J.setPartida(111l);
-		J.setPartida(P);;
+		J.setPartida(01l);
+		J.setPartida(P);
 		J.setTime_1(Tii);
 		J.setTime_2(Tii2);
 		dao3.update(J);
-		Jogo find3 = dao3.find(Jogo.class, 0001l);
+		Jogo find3 = dao3.find(Jogo.class, 02l);
 		System.out.println(find3.getId());
 
 	}
